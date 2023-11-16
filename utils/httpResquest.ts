3f46bd8640ecedeb98e60382e30b866864760230
Uche Chris <chrisuche58@yahoo.com>
2023-11-16 21:19:57 +0100

    api link changed to live api

--- utils/httpResquest.ts
+++ utils/httpResquest.ts
@@ -1,7 +1,7 @@
 import * as axios from "axios"
 // const api = 'http://localhost:3000'
 export  const  makeRequest = async (id:number) =>{
-    const response = await axios.default.get(`http://localhost:3000/risk/${id}`)
+    const response = await axios.default.get(`https://node-api-d3k7.onrender.com/risk/${id}`)
     const data = response.data
     console.log({data})
     return data
